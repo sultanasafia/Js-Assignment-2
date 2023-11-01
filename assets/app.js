68f5fee950b6415f457a471f3816d63a1b439697
sultanasafia <147336363+sultanasafia@users.noreply.github.com>
2023-11-01 10:30:08 +0500

    JS-Assignment-2

---
+++ assets/app.js
@@ -0,0 +1,31 @@
+document.write("Assignment-2")
+// Q1===================
+// let userName;
+// Q2===================
+// let myName ="Safia";
+// Q3===================
+// let message = "Hello World";
+// Q4===================
+// alert(message)
+// Q5===================
+// let userName = "Jhone Doe"
+// let age = 15
+// let course = "Web Development certified course"
+// alert(userName)
+// alert(age +" Years")
+// alert(course)
+// Q5(b)===================
+// alert('pizza\npizz\npiz\npi\np')
+// Q6=======================
+// let email = "safiabano736@gamil.com"
+// alert('My Email Address is '+ email)
+// Q7==================
+// let message ="I am trying to learn from the Book A smarter\n way to learn javascript"
+// alert(message)
+// Q8=====================
+// let message = "Yah! I can write HTML content through javascript"
+// document.write(message)
+// Q9=============
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+// ASSIGNMENT 2 COMPLETED=========================
